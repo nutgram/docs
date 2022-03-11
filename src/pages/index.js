37be6a4a6805aa26cd29f2dfe4ea0163ed171463
Luca Patera <lucapatera@outlook.it>
2022-03-11 19:24:35 +0100

    changed hero style

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,18 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-        <header className={clsx('hero hero--dark', styles.heroBanner)}>
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
-                <img src="/img/nutgram.png" width={500}/>
-                <p className="hero__subtitle hero__style">{siteConfig.tagline}</p>
+                <img src="/img/logo.svg" width={500}/>
+                <p className={clsx('hero__subtitle', styles.tagline)}>{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
                     <Link
-                        className="button button--secondary button--outline button--lg"
+                        className="button button--secondary button--lg"
                         to="/docs/introduction">
                         Getting Started
                     </Link>
                     <iframe
-                        style={{overflow: 'hidden', 'margin-left': '25px'}}
+                        style={{overflow: 'hidden', 'marginLeft': '25px'}}
                         src="https://ghbtns.com/github-btn.html?user=nutgram&amp;repo=nutgram&amp;type=star&amp;count=true&amp;size=large"
                         width={160}
                         height={30}
